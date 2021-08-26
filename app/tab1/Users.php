6157f2fb47bf3fb259c9d2056014325333b9bcb1
Gonza <gonnron@gmail.com>
2021-08-25 23:09:40 -0300

    se corrigen errores y se hace user.html,css y php

--- app/tab1/Users.php
+++ app/tab1/Users.php
@@ -1,3 +1,26 @@
 <?php
+include "../Auntenticacion.php";
 
+$Nombre=$_POST['nombre'];
+$Apellido=$_POST['apellido'];
+$Correo=$_POST['correo'];
+$Contraseña=$_POST['contraseña'];
+$Nacimiento=$_POST['fecha'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO usuarios (Nombre,Apellido,Correo,Contraseña,Nacimiento)
+VALUES ('$Nombre', '$Apellido', '$Correo','$Contraseña','$Nacimiento')";
+
+/*if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+*/
+$conn->close();
 ?>
