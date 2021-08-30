e313c0dfa6de2adb91d58d31677ff7b9229a728e
Gonza <gonnron@gmail.com>
2021-08-30 14:24:30 -0300

    modi

--- app/tab1/Users.php
+++ app/tab1/Users.php
@@ -16,11 +16,11 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO usuarios (Nombre,Apellido,Correo,Contraseña,Nacimiento)
 VALUES ('$Nombre', '$Apellido', '$Correo','$Contraseña','$Nacimiento')";
 
-/*if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
-*/
+
 $conn->close();
 ?>
